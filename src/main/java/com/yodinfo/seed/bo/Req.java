00b8172e84e0d01f1332534a615ed71165df562e
liukai <lk237429>
2019-05-10 17:29:23 +0800

    重复分页请求类

--- src/main/java/com/yodinfo/seed/bo/Req.java
+++ src/main/java/com/yodinfo/seed/bo/Req.java
@@ -1,25 +1,66 @@
 package com.yodinfo.seed.bo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yodinfo.seed.util.BeanMapUtils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Map;
 
-@ApiModel(value = "Req", description = "分页请求")
+/**
+ * 统一封装的分页请求
+ *
+ * @param <T>
+ */
+@ApiModel(value = "Req", description = "统一封装请求")
 @Getter
 @Setter
-@NoArgsConstructor
 public class Req<T> {
-    @ApiModelProperty(name = "param", value = "过滤参数")
-    private T param;
+    @ApiModelProperty(name = "filter", value = "过滤参数")
+    private T filter;
 
-    @JsonIgnore
+    @ApiModelProperty(value = "分页/排序参数")
+    @JsonProperty("pagination")
+    @JsonAlias("lazyLoadEvent") // 兼容PrimeNG TurboTable(p-Table)组件
+    private Pagination pagination;
+
     public Map<String, Object> flatAsMap() {
-        return BeanMapUtils.beanToMap(param, true);
+        Map<String, Object> paramMap = BeanMapUtils.beanToMap(filter, true);
+
+        String sortField = this.pagination.sortField;
+        if (sortField != null && sortField.length() > 0) {
+            paramMap.put("sortField", sortField);
+            paramMap.put("sortOrder", this.pagination.getSortOrder() == -1 ? "DESC" : "ASC");
+        }
+
+        Integer rows = this.pagination.getSize();
+        if (rows != null && rows != 0) {
+            paramMap.put("pageSize", rows);
+
+            Integer first = this.pagination.getOffset();
+            paramMap.put("pageNum", (first + rows) / rows);
+        }
+
+        return paramMap;
+    }
+
+    @ApiModel(value = "Pagination", description = "分页及排序")
+    @Getter
+    @Setter
+    private class Pagination {
+
+        @JsonProperty("offset")
+        @JsonAlias("first")
+        private Integer offset;
+
+        @JsonProperty("size")
+        @JsonAlias("rows")
+        private Integer size;
+
+        private String sortField;
+        private Integer sortOrder;
     }
 }
