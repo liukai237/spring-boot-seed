4114746a6d46015207c481fcdbbe8fdabd7fa3ae
Kai <kazzz@126.com>
2020-12-02 16:14:48 +0800

    新增MQ、WS配置

---
+++ midware/websocket-support/src/main/java/com/iakuil/midware/ws/WsUser.java
@@ -0,0 +1,26 @@
+package com.iakuil.midware.ws;
+
+import lombok.ToString;
+
+import javax.security.auth.Subject;
+import java.security.Principal;
+
+@ToString(of = "name")
+public class WsUser implements Principal {
+
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean implies(Subject subject) {
+        return false;
+    }
+
+    public WsUser(String name) {
+        this.name = name;
+    }
+}
